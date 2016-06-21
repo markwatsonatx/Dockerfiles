de99f216089eccc00bb94ff06913b08da6b5e844
Mark Watson <mark.watson.x@gmail.com>
2016-06-21 15:55:16 -0500

    Updated startup script

--- ionic2/scripts/startup.sh
+++ ionic2/scripts/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 rm -rf node_modules
 npm install
-ionic build
 http-server -p 8100 ./www &
-nodemon -L ./app/ --exec "ionic build"
+nodemon -L --watch ./app --ext ts,js,scss --exec "ionic build"
