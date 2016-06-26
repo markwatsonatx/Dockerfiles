d8ccac3285044c87944f95ac524e84e5c2882be8
Mark Watson <mark.watson.x@gmail.com>
2016-06-25 23:59:54 -0500

    Updates

---
+++ spark-2.0.0-preview-notebook/scripts/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$SPARK_HOME/sbin/start-master.sh
+PYSPARK_DRIVER_PYTHON=jupyter
+PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 8889 --notebook-dir='/usr/notebooks' --ip='*' --no-browser"
+$SPARK_HOME/bin/pyspark
