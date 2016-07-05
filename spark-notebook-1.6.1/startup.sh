110b6418118560fb637a0a747f112f59121da425
Mark Watson <mark.watson.x@gmail.com>
2016-07-05 15:33:58 -0500

    Updated spark notebook

--- spark-notebook-1.6.1/startup.sh
+++ spark-notebook-1.6.1/startup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-$SPARK_HOME/sbin/start-master.sh $@
-PYSPARK_DRIVER_PYTHON=jupyter \
-PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 8889 --notebook-dir='/usr/notebooks' --ip='*' --no-browser" \
-$SPARK_HOME/bin/pyspark $@
+jupyter notebook --port 8889 --notebook-dir='/usr/notebooks' --ip='*' --no-browser
