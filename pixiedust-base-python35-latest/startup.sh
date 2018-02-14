62eec1022dc892e359829c4c6fd5677620644255
Mark Watson <mark.watson.x@gmail.com>
2018-02-14 11:54:05 -0600

    Latest PixieDust for Python 2.7

--- pixiedust-base-python35-latest/startup.sh
+++ pixiedust-base-python35-latest/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jupyter notebook --port 8888 --notebook-dir='/usr/notebooks' --ip='*' --no-browser
+jupyter notebook --allow-root --port 8888 --notebook-dir='/usr/notebooks' --ip='*' --no-browser
